d3f38d3936ed18e6e3265178aea14754c1b0780f
yamori14 <nesterenkoleonid27@gmail.com>
2025-12-25 14:03:16 +0200

    фвві

--- script.js
+++ script.js
@@ -11,25 +11,8 @@ document.querySelectorAll('nav a').forEach(link => {
 });
 
 
-const toTopBtn = document.getElementById("toTopBtn");
 
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-        toTopBtn.style.display = "block";
-    } else {
-        toTopBtn.style.display = "none";
-    }
-});
-
-toTopBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
-
-
-document.getElementById("reviewForm").addEventListener("submit", function(e) {
+document.getElementById("review").addEventListener("submit", function(e) {
     e.preventDefault();
     alert("Дякуємо за ваш відгук! ❤️");
     this.reset();
